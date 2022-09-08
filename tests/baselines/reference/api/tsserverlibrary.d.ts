87dd41e085fd1f2e2533a8a77a06192fa3976ccf
Sheetal Nandi <shkamat@microsoft.com>
2022-09-08 16:17:00 -0700

    watchFactory implementation

--- tests/baselines/reference/api/tsserverlibrary.d.ts
+++ tests/baselines/reference/api/tsserverlibrary.d.ts
@@ -3403,6 +3403,7 @@ declare namespace ts {
             excludedFiles: readonly NormalizedPath[];
             updateGraph(): boolean;
             getExcludedFiles(): readonly ts.server.NormalizedPath[];
+            close(): void;
         }
         function convertFormatOptions(protocolOptions: protocol.FormatCodeSettings): FormatCodeSettings;
         function convertCompilerOptions(protocolOptions: protocol.ExternalProjectCompilerOptions): CompilerOptions & protocol.CompileOnSaveMixin;
@@ -7243,7 +7244,7 @@ declare namespace ts {
         excludeDirectories?: string[];
         excludeFiles?: string[];
         watchFactory?: string | PluginImport;
-        [option: string]: CompilerOptionsValue | undefined;
+        [option: string]: CompilerOptionsValue | Function | undefined;
     }
     interface TypeAcquisition {
         enable?: boolean;
